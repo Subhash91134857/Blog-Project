3b1a3f75be03207b25216b5da6b6716f4b5afbbd
Subhash91134857 <Subhashpanday58@gmail.com>
2023-05-14 16:37:10 +0530

    BackendUrl

--- client/src/Service/api.js
+++ client/src/Service/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { API_NOTIFICATION_MESSAGE, SERVICE_URLS } from "../constants/config"
 import { getAccessToken,getType } from "../utils/common-utils"
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://techblogapps.onrender.com';
 const axiosInstance = axios.create({
     baseURL: API_URL,
     timeout: 10000,
